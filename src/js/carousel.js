0c31f1d43fdc51f42585fc640e7a545385426149
jeanmaried <jdalmass@hotmail.com>
2017-08-01 14:30:51 -0400

    ..

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,13 +1,23 @@
-export const carousel = () => {$('.bxslider').bxSlider({
-  onSliderLoad: function(){
-    // do funky JS stuff here
-    alert('Slider has finished loading. Click OK to continue!');
-  },
-  onSlideAfter: function(){
-    // do mind-blowing JS stuff here
-    alert('A slide has finished transitioning. Bravo. Click OK to continue!');
-  }
-});
-}
+export const carousel = (data) => {
+			for (var i=0; i < data.products.length; i++){
+				console.log(i);
+				
+				if (data.products[i].categoryPath[1].name.length){
+					let image = data.products[i].largeImage;
+					let manu = data.products[i].manufacturer;
+					// let category = data.products[i].categoryPath[1].name;
+					let price = data.products[i].regularPrice;
+					let description = data.products[i].name;
+					console.log(description);
+					let div = $('<div></div>');	
+   					div.html('<div>'+manu+
+   						'</div>'+'<div>'+description+'</div>'+
+   						'<img src='+image+'>'+'<div>'+price+'</div>'+'<button>'+"ADD TO CART"+'</div>');
+					$(".here").append(div);
+				}
 
-console.log(carousel)
+				else {
+					break;
+				};
+	};
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,29 @@
 /*import Cart from "./cart";*/
-
+import Wallop from 'Wallop';
 import request from "./bestbuy";
-import carousel from "./carousel";
+import {carousel} from "./carousel";
 
-console.log(request)
-console.log(carousel)
+console.log(carousel);
 
 export default class App{
 	constructor(){
-		this.initBBCall;
+		this.initBBCall();
 	}
 	
 	initBBCall () {
-		request({url: "https://api.bestbuy.com/v1/products",api : "8ccddf4rtjz5k5btqam84qak"})
+		request({url: "https://api.bestbuy.com/v1/products(condition=new&(categoryPath.id=abcat0502000))",api : "8ccddf4rtjz5k5btqam84qak"})
 		.then(data => {
-			/* fill carosel with products */
+			carousel(data);
+
+				
+			/*  carosel with products */
+
 		})
 		.catch(error => {
 			console.log("warning Christopher Robins... Error");
 			console.log(error);
 		});
 	}
-}
+};
+
 let x = new App;
