28c59868d9d3a40fec84ee18d7ba84ad92764273
Cody Collicott <cody.collicott@redacademy.com>
2017-08-06 18:32:32 -0700

    fork merge :: changed productUtil.js to reflect new method changes

--- src/js/productUtil.js
+++ src/js/productUtil.js
@@ -17,12 +17,26 @@ export default class productUtil{
 			product.quantity = newValue;
 			sessionStorage.setItem(sku,JSON.stringify(product));
 		}
-
+		/* The problem is this line 
 		this.cartList(sku,product);
-		this.getcartItems();
+		the way you have cartList written says that every time
+		the add to cart button is clicked you should append that 
+		sku and product to the cart div
+		What you should be doing is re-looping through your session
+		to build a fresh instance of the cart every time.
+		See line 33...
+		*/
+		// you should run this.cartReBuild() here
+		this.getcartItems(); 
 
 	};
-
+	rebuildCart () {
+		/* this is where you should be looping through the session
+		and rebuilding the cart HTML based on the session  
+		1. loop through session
+		2. run your cartlist method on each of the session items.
+		*/
+	}
 	cartList(sku, product){
 		let cartItem = $('<div class="itemRows"></div>');
 		cartItem.html('<div>'+'SKU'+'</div>'+'<div>'+sku+'</div>'+
